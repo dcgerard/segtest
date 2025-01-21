f8b23161052d6d0baa956b6b10c2b17f9e47b6ca
dcgerard <gerard.1787@gmail.com>
2025-01-21 15:58:10 -0500

    more tests

--- tests/testthat/test-higher_freq.R
+++ tests/testthat/test-higher_freq.R
@@ -2,18 +2,38 @@ test_that("expit and logit are inverses", {
   expect_equal(logit(expit(c(0.3, 0.4))), c(0.3, 0.4))
 })
 
-testthat("real_to_simplex and simplex_to_real are inverses", {
+test_that("real_to_simplex and simplex_to_real are inverses", {
   expect_equal(simplex_to_real(real_to_simplex(c(1, 3))), c(1, 3))
 })
 
-testthat("offspring_gf_2 and f1_gf_dr are same for tetraploids", {
+test_that("offspring_gf_2 and f1_gf_dr are same for tetraploids", {
   expect_equal(
     offspring_gf_2(alpha = 1/12, xi1 = 1/3, xi2 = 1/3, p1 = 2, p2 = 1),
     f1_gf_dr(alpha = 1/12, g1 = 2, g2 = 1, ploidy = 4)
   )
 
   expect_equal(
-    offspring_gf_2(alpha = 1/12, xi1 = 1/3, xi2 = 1/3, p1 = 1, p2 = 3),
-    f1_gf_dr(alpha = 1/12, g1 = 1, g2 = 3, ploidy = 4)
+    pvec_tet_2(alpha = 1/12, xi = 1/3, ell = 0),
+    gamfreq_dr(alpha = 1/12, g = 0, ploidy = 4, log_p = FALSE)
+  )
+
+  expect_equal(
+    pvec_tet_2(alpha = 1/12, xi = 1/3, ell = 1),
+    gamfreq_dr(alpha = 1/12, g = 1, ploidy = 4, log_p = FALSE)
+  )
+
+  expect_equal(
+    pvec_tet_2(alpha = 1/12, xi = 1/3, ell = 2),
+    gamfreq_dr(alpha = 1/12, g = 2, ploidy = 4, log_p = FALSE)
+  )
+
+  expect_equal(
+    pvec_tet_2(alpha = 1/12, xi = 1/3, ell = 3),
+    gamfreq_dr(alpha = 1/12, g = 3, ploidy = 4, log_p = FALSE)
+  )
+
+  expect_equal(
+    pvec_tet_2(alpha = 1/12, xi = 1/3, ell = 4),
+    gamfreq_dr(alpha = 1/12, g = 4, ploidy = 4, log_p = FALSE)
   )
 })
